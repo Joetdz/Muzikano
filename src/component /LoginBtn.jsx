76eb81e5f725cfdcfcdd482700e6cf3facb8273b
Joel tondozi <joeltondozi@gmail.com>
2022-08-25 19:11:15 +0200

    Refactor : remove google logo in authentification page

--- src/component /LoginBtn.jsx
+++ src/component /LoginBtn.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const LoginBtn = ({href}) => {
     return (
         <div className="login-btn">
-            <div className="google-img">
-                <img src="img/google.png" alt="" />
-            </div>
+           
             <div className="login-link">
                <a href={href}><button>Connectez vous </button></a> 
             </div>
